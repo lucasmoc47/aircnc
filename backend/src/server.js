8065e236cb90ab4e86a0523eca77c5afdd5b2992
Lucas <lucasmoc47@gmail.com>
2020-02-27 21:57:21 -0300

    Adding mongoose and connecting to mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const routes = require('./routes')
 
 const app = express()
+
+mongoose.connect('mongodb+srv://lucas:OHnt8AMFXTNnBYhc@omnistack-4t7ay.mongodb.net/aircnc?retryWrites=true&w=majority', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
 
 app.use(express.json())
 app.use(routes)

37c1e78a2a3aa6e7e1e8779d3b7ed41f85cd7868
Lucas <lucasmoc47@gmail.com>
2020-02-29 20:39:58 -0300

    Saving data in local storage and setting up List view

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,8 +1,39 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, AsyncStorage, Image, StyleSheet } from 'react-native'
+
+import logo from '../assets/logo.png'
+
+import SpotList from '../components/SpotList'
 
 export default function List() {
+	const [techs, setTechs] = useState([])
+
+	useEffect(() => {
+		AsyncStorage.getItem('techs').then(storagedTechs => {
+			const techsArray = storagedTechs.split(',').map(tech => tech.trim())
+
+			setTechs(techsArray)
+		})
+	}, [])
+
 	return (
-		<View />
+		<View style={styles.container}>
+			<Image source={logo} style={styles.logo} />
+			
+			{techs.map((tech, index) => <SpotList key={index} tech={tech} />)}
+		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	}, 
+
+	logo: {
+		height: 32,
+		resizeMode: 'contain',
+		alignSelf: 'center',
+		marginTop: 50
+	}
+})
--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,34 +1,73 @@
-import React from 'react'
-import { View, KeyboardAvoidingView,Image, Platform, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import {
+	View,
+	KeyboardAvoidingView,
+	Image,
+	Platform,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	StyleSheet,
+	AsyncStorage
+} from 'react-native'
+
+import api from '../services/api'
 
 import logo from '../assets/logo.png'
 
-export default function Login() {
+export default function Login({ navigation }) {
+	const [email, setEmail] = useState('')
+	const [techs, setTechs] = useState('')
+
+	useEffect(() => {
+		AsyncStorage.getItem('user').then(user => {
+			if(user)
+				navigation.navigate('List')
+		})
+	}, [])
+
+	async function handleSubmit() {
+		const response = await api.post('/sessions', {
+			email
+		})
+
+		const { _id } = response.data
+
+		await AsyncStorage.setItem('user', _id)
+		await AsyncStorage.setItem('techs', techs)
+
+		navigation.navigate('List')
+	}
+
 	return (
 		<KeyboardAvoidingView enabled={Platform.OS === 'ios'} behavior="padding" style={styles.container}>
 			<Image source={logo} />
 
 			<View style={styles.form}>
 				<Text style={styles.label}>SEU EMAIL *</Text>
-				<TextInput 
+				<TextInput
 					style={styles.input}
 					placeholder="Seu e-mail"
 					placeholderTextColor="#999"
 					keyboardType="email-address"
 					autoCapitalize="none"
 					autoCorrect={false}
+					value={email}
+					onChangeText={setEmail}
 				/>
 
 				<Text style={styles.label}>TECNOLOGIAS *</Text>
-				<TextInput 
+				<TextInput
 					style={styles.input}
 					placeholder="Tecnologias de interesse"
 					placeholderTextColor="#999"
 					autoCapitalize="words"
 					autoCorrect={false}
+					value={techs}
+					onChangeText={setTechs}
 				/>
 
-				<TouchableOpacity style={styles.button}>
+				<TouchableOpacity style={styles.button} onPress={handleSubmit}>
 					<Text style={styles.buttonText}>Encontrar spots</Text>
 				</TouchableOpacity>
 			</View>

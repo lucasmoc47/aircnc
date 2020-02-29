e1e17b744f6d8b9d30203c0eed24293bc2ac1b86
Lucas <lucasmoc47@gmail.com>
2020-02-29 13:28:52 -0300

    Finishing the dashboard page

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import api from '../../services/api'
 
 import './styles.css'
@@ -9,7 +11,7 @@ export default function Dashboard() {
 	useEffect(() => {
 		async function loadSpots() {
 			const user_id = localStorage.getItem('user')
-			
+
 			const response = await api.get('/dashboard', {
 				headers: { user_id }
 			})
@@ -17,18 +19,22 @@ export default function Dashboard() {
 			setSpots(response.data)
 		}
 
-		loadSpots() 
+		loadSpots()
 	}, [])
 
 	return (
-		<ul className="spot-list">
-			{spots.map(spot => (
-				<li key={spot._id}>
-					<header style={{ backgroundImage: `url(${spot.thumbnail_url})` }}/>
-					<strong>{spot.company}</strong>
-					<span>{spot.price}</span>
-				</li>
-			))}
-		</ul>
+		<>
+			<ul className="spot-list">
+				{spots.map(spot => (
+					<li key={spot._id}>
+						<header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
+						<strong>{spot.company}</strong>
+						<span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO'}</span>
+					</li>
+				))}
+			</ul>
+
+			<Link to="/new"><button className="btn">Cadastrar novo spot</button></Link>
+		</>
 	);
 }
